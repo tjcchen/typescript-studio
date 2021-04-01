0b47227aaffcc0c4ebaa6183c7779fe4f606df86
Andy Chen <chenyangpoppin@hotmail.com>
2021-04-01 23:52:21 +0800

    feat: basic types of typescript phase 1

---
+++ src/basic/basicTypes.js
@@ -0,0 +1,15 @@
+// Basic typescript types
+// 1. Booelan
+var isDone = false;
+// 2. Number
+var decimal = 6;
+var hex = 0xf00d;
+var binary = 10;
+var octal = 484;
+// let big: bigint = 100n;
+// 3. String
+var color = 'blue';
+color = 'red';
+var fullname = 'Kobe Bryant';
+var age = 37;
+var sentence = "Hello, my name is " + fullname + "\n  I'll be " + (age + 1) + " years old next month.\n";
---
+++ src/basic/basicTypes.ts
@@ -0,0 +1,21 @@
+// Basic typescript types
+
+// 1. Booelan
+let isDone: boolean = false;
+
+// 2. Number
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+// let big: bigint = 100n;
+
+// 3. String
+let color: string = 'blue';
+color = 'red';
+
+let fullname: string = 'Kobe Bryant';
+let age: number = 37;
+let sentence: string = `Hello, my name is ${fullname}
+  I'll be ${age + 1} years old next month.
+`;
