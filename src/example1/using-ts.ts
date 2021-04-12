59c774a5a40878a3f0ad6207d22ca2d44203e42a
Andy Chen <chenyangpoppin@hotmail.com>
2021-04-12 08:53:30 +0800

    chore: format thing

--- src/example1/using-ts.ts
+++ src/example1/using-ts.ts
@@ -2,10 +2,10 @@ const button = document.querySelector('#btn');
 const input1 = document.querySelector('#num1')! as HTMLInputElement;
 const input2 = document.querySelector('#num2')! as HTMLInputElement;
 
-const add = (num1: number, num2: number) => {
+const add = (num1: number, num2: number): number => {
   return num1 + num2;
 };
 
-button.addEventListener('click', () => {
+button.addEventListener('click', (): void => {
   console.log(add(+input1.value, +input2.value));
 }, false);
