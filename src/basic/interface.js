0ba90c3d0c06b940c0eca519be955dc4165b0f5d
Andy Chen <chenyangpoppin@hotmail.com>
2021-05-15 23:12:35 +0800

    feat: rewrite first interface with interface syntax

--- src/basic/interface.js
+++ src/basic/interface.js
@@ -1,7 +1,9 @@
 // Interfaces
 // One of TypeScript's core principle is that type checking focuses on the shape that values have.
 // This is something called "duck typing" or "structural subtyping"
+//----------------
 // First interface
+//----------------
 function printLabel(labeledObj) {
     console.log(labeledObj.label);
 }
@@ -9,4 +11,8 @@ var myObj = {
     size: 10,
     label: "Size 10 Object"
 };
-printLabel(myObj);
+printLabel(myObj); // print out "Size 10 Object"
+function printLabel2(labeledObj) {
+    console.log(labeledObj.label);
+}
+printLabel2(myObj);
--- src/basic/interface.ts
+++ src/basic/interface.ts
@@ -2,7 +2,9 @@
 // One of TypeScript's core principle is that type checking focuses on the shape that values have.
 // This is something called "duck typing" or "structural subtyping"
 
+//----------------
 // First interface
+//----------------
 function printLabel(labeledObj: { label: string }) {
   console.log(labeledObj.label);
 }
@@ -13,3 +15,16 @@ let myObj = {
 };
 
 printLabel(myObj); // print out "Size 10 Object"
+
+//------------------------
+// rewrite first interface
+//------------------------
+interface LabeledValue {
+  label: string
+}
+
+function printLabel2(labeledObj: LabeledValue) {
+  console.log(labeledObj.label);
+}
+
+printLabel2(myObj); // print out "Size 10 Object"
