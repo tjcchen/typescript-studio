a3d827bd2b2ad325234afb452631b16c8a1175f4
Andy Chen <chenyangpoppin@hotmail.com>
2021-05-15 23:27:15 +0800

    feat: add an example concerning interface optional properties

--- src/basic/interface.js
+++ src/basic/interface.js
@@ -15,4 +15,16 @@ printLabel(myObj); // print out "Size 10 Object"
 function printLabel2(labeledObj) {
     console.log(labeledObj.label);
 }
-printLabel2(myObj);
+printLabel2(myObj); // print out "Size 10 Object"
+function createSquare(config) {
+    var newSquare = { color: "white", area: 100 };
+    if (config.color) {
+        newSquare.color = config.color;
+    }
+    if (config.width) {
+        newSquare.area = config.width * config.width;
+    }
+    return newSquare;
+}
+var mySquare = createSquare({ color: "black" });
+console.log(mySquare);
--- src/basic/interface.ts
+++ src/basic/interface.ts
@@ -20,7 +20,7 @@ printLabel(myObj); // print out "Size 10 Object"
 // rewrite first interface
 //------------------------
 interface LabeledValue {
-  label: string
+  label: string;
 }
 
 function printLabel2(labeledObj: LabeledValue) {
@@ -28,3 +28,25 @@ function printLabel2(labeledObj: LabeledValue) {
 }
 
 printLabel2(myObj); // print out "Size 10 Object"
+
+//--------------------
+// Optional properties
+//--------------------
+interface SquareConfig {
+  color?: string;
+  width?: number;
+}
+
+function createSquare(config: SquareConfig): { color: string; area: number } {
+  let newSquare = { color: "white", area: 100 };
+  if (config.color) {
+    newSquare.color = config.color;
+  }
+  if (config.width) {
+    newSquare.area = config.width * config.width;
+  }
+  return newSquare;
+}
+
+let mySquare = createSquare({ color: "black" });
+console.log(mySquare); // { color: 'black', area: 100 }
